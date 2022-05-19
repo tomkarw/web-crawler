af3045345fafad6201c4747aba76df28b28dfc96
Tomek Karwowski <to.karwowski@gmail.com>
2022-05-19 22:26:48 +0200

    timeout crawl_page and send join handles via channel isntead of queue

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,21 @@
 use clap::{arg, Arg, Command};
-use concurrent_queue::ConcurrentQueue;
 use reqwest::Url;
 use scraper::{Html, Selector};
 use select::document::Document;
 use select::predicate::Name;
 use std::collections::{HashMap, HashSet};
 use std::error::Error;
-use std::io;
-use std::io::Write;
 use std::sync::atomic::{AtomicUsize, Ordering};
-use std::sync::Arc;
+use std::sync::{Arc, RwLock};
 use std::time::Duration;
-use tokio::sync::{RwLock};
+use tokio::sync::mpsc::Sender;
 use tokio::task::JoinHandle;
+use tokio::time::{timeout, Timeout};
 
 type QueryMatches = Arc<RwLock<HashMap<Url, String>>>;
 
 const TIMEOUT: Duration = Duration::from_secs(5);
+const QUEUE_SIZE: usize = 32;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
@@ -58,38 +57,43 @@ async fn main() -> Result<(), Box<dyn Error>> {
     let link_count = Arc::new(AtomicUsize::new(0));
     let query_matches = QueryMatches::new(Default::default());
 
-    let handle_queue = Arc::new(ConcurrentQueue::unbounded());
-    handle_queue.push(crawl_page(
-        Url::parse(url)?,
-        query.to_string(),
-        depth,
-        range,
-        link_count.clone(),
-        query_matches.clone(),
-        handle_queue.clone(),
-    )).unwrap();
-
-    while let Ok(handle) = handle_queue.pop() {
-        tokio::join!(handle).0.unwrap();
-    }
-
-    let stdout = io::stdout();
-    let mut stdout = stdout.lock();
-    stdout.write_all(
-        (&format!(
-            "Crawled {} pages. Found {} pages with the term `{}`.\n",
-            link_count.load(Ordering::SeqCst),
-            query_matches.read().await.len(),
-            query
+    let (sender, mut receiver) = tokio::sync::mpsc::channel(QUEUE_SIZE);
+    sender
+        .send(crawl_page(
+            Url::parse(url)?,
+            query.to_string(),
+            depth,
+            range,
+            link_count.clone(),
+            query_matches.clone(),
+            sender.clone(),
         ))
-            .as_ref(),
-    )?;
-    for (url, snippet) in query_matches.read().await.iter() {
-        stdout.write_all((&format!("{} => '{}'\n", url, snippet.trim())).as_ref())?;
+        .await?;
+    drop(sender);
+
+    while let Some(handle) = receiver.recv().await {
+        let error = match tokio::join!(handle).0 {
+            Ok(Ok(Ok(()))) => None,
+            Ok(Ok(Err(e))) => Some(Box::new(e)),
+            Ok(Err(e)) => Some(Box::new(e.into())),
+            Err(e) => Some(Box::new(e.into())),
+        };
+        if let Some(error) = error {
+            println!("Failed to parse page: {error}");
+        }
     }
+
+    println!(
+        "Crawled {} pages. Found {} pages with the term `{}`.",
+        link_count.load(Ordering::SeqCst),
+        query_matches.read().unwrap().len(),
+        query
+    );
 
     Ok(())
 }
+
+type CrawlPageReturn = Timeout<JoinHandle<anyhow::Result<()>>>;
 
 fn crawl_page(
     url: Url,
@@ -98,78 +102,83 @@ fn crawl_page(
     range: usize,
     link_count: Arc<AtomicUsize>,
     query_matches: QueryMatches,
-    handle_queue: Arc<ConcurrentQueue<JoinHandle<()>>>,
-) -> JoinHandle<()> {
-    tokio::spawn(async move {
-        if depth == 0 {
-            return; // Ok(());
-        }
-
-        // TODO: make sure what ordering is needed
-        link_count.fetch_add(1, Ordering::AcqRel);
-
-        let response = reqwest::get(url.clone()).await.unwrap();
-        // TODO: handle redirects
-        if !response.status().is_success() {
-            return; // Ok(());
-        }
-        let body = response.text().await.unwrap();
-
-        if let Some(matched) = find_query(&body, &query, range) {
-            query_matches.write().await.insert(url.clone(), matched);
-        }
-
-        let links = Document::from(body.as_str())
-            .find(Name("a"))
-            .filter_map(|anchor| anchor.attr("href"))
-            .filter_map(|raw_url| {
-                match Url::parse(raw_url) {
-                    Ok(new_url) => {
-                        // ignore pages on different domain
-                        if new_url.host() == url.host() {
-                            Some(new_url)
-                        } else {
-                            None
-                        }
-                    }
-                    Err(_) => {
-                        // TODO: could there be relative links?
-                        if raw_url.starts_with('/') {
-                            url.join(raw_url).ok()
-                        } else {
-                            None
-                        }
-                    }
-                }
-            })
-            .collect::<HashSet<_>>();
-
-        for link in links {
-            // don't visit already visited links
-            if query_matches.read().await.contains_key(&link) {
-                continue;
+    sender: Sender<CrawlPageReturn>,
+) -> CrawlPageReturn {
+    timeout(
+        TIMEOUT,
+        tokio::spawn(async move {
+            if depth == 0 {
+                return Ok(());
             }
 
-            // link_queue_sender.send((link, depth - 1))?;
-            handle_queue.push(crawl_page(
-                link,
-                query.clone(),
-                depth - 1,
-                range,
-                link_count.clone(),
-                query_matches.clone(),
-                handle_queue.clone(),
-            )).unwrap();
-        }
+            // TODO: make sure what ordering is needed
+            link_count.fetch_add(1, Ordering::AcqRel);
 
-        // Ok(())
-    })
+            let response = reqwest::get(url.clone()).await?;
+            // TODO: handle redirects
+            if !response.status().is_success() {
+                return Ok(());
+            }
+            let body = response.text().await?;
+
+            if let Some(matched) = find_query(&body, &query, range) {
+                println!("{} => '{}'", url, matched.trim());
+                query_matches.write().unwrap().insert(url.clone(), matched);
+            }
+
+            let links = Document::from(body.as_str())
+                .find(Name("a"))
+                .filter_map(|anchor| anchor.attr("href"))
+                .filter_map(|raw_url| {
+                    match Url::parse(raw_url) {
+                        Ok(new_url) => {
+                            // ignore pages on different domain
+                            if new_url.host() == url.host() {
+                                Some(new_url)
+                            } else {
+                                None
+                            }
+                        }
+                        Err(_) => {
+                            // TODO: could there be relative links?
+                            if raw_url.starts_with('/') {
+                                url.join(raw_url).ok()
+                            } else {
+                                None
+                            }
+                        }
+                    }
+                })
+                .collect::<HashSet<_>>();
+
+            for link in links {
+                // don't visit already visited links
+                if query_matches.read().unwrap().contains_key(&link) {
+                    continue;
+                }
+
+                // link_queue_sender.send((link, depth - 1))?;
+                sender
+                    .send(crawl_page(
+                        link,
+                        query.clone(),
+                        depth - 1,
+                        range,
+                        link_count.clone(),
+                        query_matches.clone(),
+                        sender.clone(),
+                    ))
+                    .await?;
+            }
+            Ok(())
+        }),
+    )
 }
 
 // TODO: only search visible text
 fn find_query(html: &str, query: &str, range: usize) -> Option<String> {
     let fragment = Html::parse_fragment(html);
-    let selector = Selector::parse("html").unwrap();
+    let selector = Selector::parse("html").expect("no html element");
     let body = match fragment.select(&selector).next() {
         None => return None,
         Some(body) => body,
@@ -186,6 +195,9 @@ fn find_query(html: &str, query: &str, range: usize) -> Option<String> {
         } else {
             cursor + query.len() + range
         };
-        text[start..end].to_string()
+        text.chars()
+            .skip(start)
+            .take(end - start)
+            .collect::<String>()
     })
 }
